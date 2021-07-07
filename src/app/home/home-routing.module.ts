a1935f6275787bb216b394cc8c2b7c6ac7686d77
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-07-07 18:49:05 +0100

    Created main menu folder

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,6 +6,10 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
+  },
+  {
+    path: 'main-menu',
+    loadChildren: () => import('./main-menu/main-menu.module').then( m => m.MainMenuPageModule)
   }
 ];
 
