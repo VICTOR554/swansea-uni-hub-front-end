35be75f06048193821bbb9d1a9745a4e27c86e25
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-07-07 20:01:15 +0100

    Created tasks folder
    using ionic generate

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -40,6 +40,10 @@ const routes: Routes = [
     path: 'notes',
     loadChildren: () => import('./notes/notes.module').then( m => m.NotesPageModule)
   },
+  {
+    path: 'tasks',
+    loadChildren: () => import('./tasks/tasks.module').then( m => m.TasksPageModule)
+  },
 ];
 
 @NgModule({
