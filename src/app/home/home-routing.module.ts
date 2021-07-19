08dca98091003b50bb1b09850fc7a672d6a22580
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-07-19 18:34:08 +0100

    updated home routing module with the new children routes

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -17,47 +17,60 @@ const routes: Routes = [
           {
             path: 'student-information',
             // eslint-disable-next-line max-len
-            loadChildren: () => import('./main-menu/student-information/student-information.module').then( m => m.StudentInformationPageModule)
+            loadChildren: () => import('./main-menu/student-information/student-information.module').then(m => m.StudentInformationPageModule)
           },
           {
             path: 'submission-deadline',
             // eslint-disable-next-line max-len
-            loadChildren: () => import('./main-menu/submission-deadline/submission-deadline.module').then( m => m.SubmissionDeadlinePageModule)
+            loadChildren: () => import('./main-menu/submission-deadline/submission-deadline.module').then(m => m.SubmissionDeadlinePageModule)
           },
           {
             path: 'library',
-            loadChildren: () => import('./main-menu/library/library.module').then( m => m.LibraryPageModule)
+            loadChildren: () => import('./main-menu/library/library.module').then(m => m.LibraryPageModule)
           },
           {
             path: 'maps',
-            loadChildren: () => import('./main-menu/maps/maps.module').then( m => m.MapsPageModule)
+            loadChildren: () => import('./main-menu/maps/maps.module').then(m => m.MapsPageModule)
           },
           {
             path: 'booking',
-            loadChildren: () => import('./main-menu/booking/booking.module').then( m => m.BookingPageModule)
+            loadChildren: () => import('./main-menu/booking/booking.module').then(m => m.BookingPageModule)
           },
           {
             path: 'student-union',
-            loadChildren: () => import('./main-menu/student-union/student-union.module').then( m => m.StudentUnionPageModule)
+            loadChildren: () => import('./main-menu/student-union/student-union.module').then(m => m.StudentUnionPageModule)
           },
           {
             path: 'event',
-            loadChildren: () => import('./main-menu/event/event.module').then( m => m.EventPageModule)
+            loadChildren: () => import('./main-menu/event/event.module').then(m => m.EventPageModule)
           },
           {
             path: 'key-contact',
-            loadChildren: () => import('./main-menu/key-contact/key-contact.module').then( m => m.KeyContactPageModule)
+            loadChildren: () => import('./main-menu/key-contact/key-contact.module').then(m => m.KeyContactPageModule)
           },
           {
             path: 'report',
-            loadChildren: () => import('./main-menu/report/report.module').then( m => m.ReportPageModule)
+            loadChildren: () => import('./main-menu/report/report.module').then(m => m.ReportPageModule)
           }
         ]
 
       },
       {
         path: 'timetable',
-        loadChildren: () => import('./timetable/timetable.module').then(m => m.TimetablePageModule)
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./timetable/timetable.module').then(m => m.TimetablePageModule)
+          },
+          {
+            path: 'list-view',
+            loadChildren: () => import('./timetable/list-view/list-view.module').then(m => m.ListViewPageModule)
+          },
+          {
+            path: 'grid-view',
+            loadChildren: () => import('./timetable/grid-view/grid-view.module').then(m => m.GridViewPageModule)
+          }
+        ]
       },
       {
         path: 'notification',
@@ -65,7 +78,20 @@ const routes: Routes = [
       },
       {
         path: 'notes',
-        loadChildren: () => import('./notes/notes.module').then(m => m.NotesPageModule)
+        children: [
+          {
+            path: '',
+            loadChildren: () => import('./notes/notes.module').then(m => m.NotesPageModule)
+          },
+          {
+            path: 'new-notes',
+            loadChildren: () => import('./notes/new-notes/new-notes.module').then( m => m.NewNotesPageModule)
+          },
+          {
+            path: 'edit-notes',
+            loadChildren: () => import('./notes/edit-notes/edit-notes.module').then( m => m.EditNotesPageModule)
+          }
+        ]
       },
       {
         path: 'tasks',
--- src/app/home/main-menu/main-menu-routing.module.ts
+++ src/app/home/main-menu/main-menu-routing.module.ts
@@ -8,6 +8,42 @@ const routes: Routes = [
     path: '',
     component: MainMenuPage
   },
+  {
+    path: 'student-information',
+    loadChildren: () => import('./student-information/student-information.module').then( m => m.StudentInformationPageModule)
+  },
+  {
+    path: 'submission-deadline',
+    loadChildren: () => import('./submission-deadline/submission-deadline.module').then( m => m.SubmissionDeadlinePageModule)
+  },
+  {
+    path: 'library',
+    loadChildren: () => import('./library/library.module').then( m => m.LibraryPageModule)
+  },
+  {
+    path: 'maps',
+    loadChildren: () => import('./maps/maps.module').then( m => m.MapsPageModule)
+  },
+  {
+    path: 'booking',
+    loadChildren: () => import('./booking/booking.module').then( m => m.BookingPageModule)
+  },
+  {
+    path: 'student-union',
+    loadChildren: () => import('./student-union/student-union.module').then( m => m.StudentUnionPageModule)
+  },
+  {
+    path: 'event',
+    loadChildren: () => import('./event/event.module').then( m => m.EventPageModule)
+  },
+  {
+    path: 'key-contact',
+    loadChildren: () => import('./key-contact/key-contact.module').then( m => m.KeyContactPageModule)
+  },
+  {
+    path: 'report',
+    loadChildren: () => import('./report/report.module').then( m => m.ReportPageModule)
+  }
 ];
 
 @NgModule({
