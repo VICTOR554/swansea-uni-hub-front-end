a4c60b4002b458fa646e47da5f66d165b651cda2
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-07-23 10:29:37 +0100

    made in progress, complete, flagged and overdue tasks a child page of tasks and utilised the ion tab to make them overlap on the same page.

--- src/app/home/tasks/tasks-routing.module.ts
+++ src/app/home/tasks/tasks-routing.module.ts
@@ -6,32 +6,34 @@ import { TasksPage } from './tasks.page';
 const routes: Routes = [
   {
     path: '',
-    component: TasksPage
+    component: TasksPage,
+    children: [
+      {
+        path: 'new-tasks',
+        loadChildren: () => import('./new-tasks/new-tasks.module').then( m => m.NewTasksPageModule)
+      },
+      {
+        path: 'edit-tasks',
+        loadChildren: () => import('./edit-tasks/edit-tasks.module').then( m => m.EditTasksPageModule)
+      },
+      {
+        path: 'in-progress-tasks',
+        loadChildren: () => import('./in-progress-tasks/in-progress-tasks.module').then( m => m.InProgressTasksPageModule)
+      },
+      {
+        path: 'completed-tasks',
+        loadChildren: () => import('./completed-tasks/completed-tasks.module').then( m => m.CompletedTasksPageModule)
+      },
+      {
+        path: 'overdue-tasks',
+        loadChildren: () => import('./overdue-tasks/overdue-tasks.module').then( m => m.OverdueTasksPageModule)
+      },
+      {
+        path: 'flagged-tasks',
+        loadChildren: () => import('./flagged-tasks/flagged-tasks.module').then( m => m.FlaggedTasksPageModule)
+      },
+    ]
   },
-  {
-    path: 'new-tasks',
-    loadChildren: () => import('./new-tasks/new-tasks.module').then( m => m.NewTasksPageModule)
-  },
-  {
-    path: 'edit-tasks',
-    loadChildren: () => import('./edit-tasks/edit-tasks.module').then( m => m.EditTasksPageModule)
-  },
-  {
-    path: 'in-progress-tasks',
-    loadChildren: () => import('./in-progress-tasks/in-progress-tasks.module').then( m => m.InProgressTasksPageModule)
-  },
-  {
-    path: 'completed-tasks',
-    loadChildren: () => import('./completed-tasks/completed-tasks.module').then( m => m.CompletedTasksPageModule)
-  },
-  {
-    path: 'overdue-tasks',
-    loadChildren: () => import('./overdue-tasks/overdue-tasks.module').then( m => m.OverdueTasksPageModule)
-  },
-  {
-    path: 'flagged-tasks',
-    loadChildren: () => import('./flagged-tasks/flagged-tasks.module').then( m => m.FlaggedTasksPageModule)
-  }
 ];
 
 @NgModule({
