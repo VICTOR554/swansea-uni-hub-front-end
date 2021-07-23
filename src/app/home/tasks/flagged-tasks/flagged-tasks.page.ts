12774d43b1a8e338baa328c6f95560f80fb344d8
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-07-23 21:02:35 +0100

    Created a flagged tasks list and used ngfor to and string interpolation to call the data from the array

--- src/app/home/tasks/flagged-tasks/flagged-tasks.page.ts
+++ src/app/home/tasks/flagged-tasks/flagged-tasks.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Flagged } from '../tasks.model';
+import { TasksService } from '../tasks.service';
 
 @Component({
   selector: 'app-flagged-tasks',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./flagged-tasks.page.scss'],
 })
 export class FlaggedTasksPage implements OnInit {
+  loadedFlaggedTasks: Flagged [];
 
-  constructor() { }
+  constructor(private taskService: TasksService) { }
 
   ngOnInit() {
+    this.loadedFlaggedTasks = this.taskService.flagged;
   }
 
 }
