db27b0cc117395d28f07927f80d37b36de94b221
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-07-26 01:03:43 +0100

    created data model for the array. Also created the array and cloned the array to make sure it cannot be changed

--- src/app/home/main-menu/report/report.service.ts
+++ src/app/home/main-menu/report/report.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Report } from './report.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportService {
 
+  private _report: Report [] = [
+    new Report(
+      1,
+      'Maps',
+      'maps does not work',
+    ),
+    new Report(
+      2,
+      'Timetable',
+      'timetable is trash',
+    ),
+    new Report(
+      3,
+      'Notes',
+      'Notes is trash',
+    ),
+    new Report(
+      4,
+      'Event',
+      'Event is trash',
+    ),
+  ];
+
+  get notes() {
+    // eslint-disable-next-line no-underscore-dangle
+    return [...this._report];
+  }
   constructor() { }
 }
