355f5d047eda7b81b627af85bceb8b11a90d58de
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-08-24 22:30:15 +0100

    Created reactive forms with validators for new tasks page

--- src/app/home/tasks/new-tasks/new-tasks.page.ts
+++ src/app/home/tasks/new-tasks/new-tasks.page.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-new-tasks',
   templateUrl: './new-tasks.page.html',
   styleUrls: ['./new-tasks.page.scss'],
 })
 export class NewTasksPage implements OnInit {
+  form: FormGroup;
+  date;
+  latestDate;
 
   constructor() { }
 
   ngOnInit() {
+    this.date = new Date().toISOString();
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required],
+      }),
+      moduleCode: new FormControl(null, {
+        updateOn: 'blur',
+      }),
+      dueDate: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required],
+      }),
+      body: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(1800)],
+      }),
+    });
   }
-
+  onCreateTask() {
+    console.log(this.form);
+  }
 }
