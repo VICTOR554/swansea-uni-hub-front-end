44ae0f1942d9c1963dfcba5aafaf88e1c87005d5
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-08-26 00:15:40 +0100

    Created reactive forms with validators for edit tasks page

--- src/app/home/tasks/edit-tasks/edit-tasks.page.ts
+++ src/app/home/tasks/edit-tasks/edit-tasks.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-tasks',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-tasks.page.scss'],
 })
 export class EditTasksPage implements OnInit {
-
+  form: FormGroup;
+  date;
   constructor() { }
 
   ngOnInit() {
+    this.date = new Date().toISOString();
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required],
+      }),
+      moduleCode: new FormControl(null, {
+        updateOn: 'blur',
+      }),
+      dueDate: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required],
+      }),
+      descripton: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.maxLength(1800)],
+      }),
+    });
   }
 
+  onUpdateTask() {
+    console.log(this.form);
+  }
 }
