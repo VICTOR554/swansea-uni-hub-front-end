e2f0a63f4298e4adf803ad0bf68383c1ad0e9174
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-18 21:57:29 +0000

    updated library model, service and html page with new data added to the array

--- src/app/home/main-menu/library/library.model.ts
+++ src/app/home/main-menu/library/library.model.ts
@@ -3,6 +3,10 @@ export class Library {
     public id: number,
     public title: string,
     public author: string,
+    public subject: string,
+    public publisher: string,
+    public createdAt: string,
     public availableAt: string,
+
     ) {}
 }
--- src/app/home/main-menu/library/library.service.ts
+++ src/app/home/main-menu/library/library.service.ts
@@ -11,24 +11,36 @@ export class LibraryService {
       1,
       'Software Engineering',
       'Sommerville, Ian, 1951',
+      'Software Engineering',
+      'Boston : Pearson',
+      '2011',
       'Bay & Singleton'
     ),
     new Library(
       2,
       'Mechanical Engineering',
-      'Steven, Ubiverse, 1951',
+      'Steven, Universe, 1951',
+      'Mechanical Engineering',
+      'Boston : Pearson',
+      '2018',
       'Singleton'
     ),
     new Library(
       3,
       'Medicap practice',
       'Jack, Anderson, 1959',
+      'Medicine',
+      'Boston : Pearson',
+      '2010',
       'Bay'
     ),
     new Library(
       4,
       'Cryptography',
       'Dave, Kensington, 1950',
+      'Software Engineering',
+      'Boston : Pearson',
+      '2020',
       'Bay & Singleton'
     ),
 
