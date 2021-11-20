fcd7a0098739396999ea2970e4c9bca21e7673f2
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-20 16:15:57 +0000

    Created student union model

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private _userIsAuthenticated = false;
+  private _userIsAuthenticated = true;
 
   get userIsAuthenticated() {
     // eslint-disable-next-line no-underscore-dangle
---
+++ src/app/home/main-menu/student-union/student-union.model.ts
@@ -0,0 +1,8 @@
+export class StudentUnion {
+  constructor(
+    public id: number,
+    public icon: string,
+    public title: string,
+    public path: string,
+    ) {}
+}
