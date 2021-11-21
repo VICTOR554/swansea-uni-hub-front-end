c669ea92774022bed7238364a2218d06ba4c3024
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-21 19:07:49 +0000

    added schema to event model

---
+++ src/app/home/main-menu/student-union/event/event.model.ts
@@ -0,0 +1,14 @@
+export class Event {
+  constructor(
+    public id: number,
+    public img: string,
+    public title: string,
+    public category: string,
+    public genre: string,
+    public description: string,
+    public price: number,
+    public location: string,
+    ) {}
+}
+
+
