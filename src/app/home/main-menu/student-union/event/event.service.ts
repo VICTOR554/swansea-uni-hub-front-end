61fa15476aca64d34a2553a0a41ac2763dc1dfc1
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-21 19:31:41 +0000

    created an event array with data based on the model

--- src/app/home/main-menu/student-union/event/event.service.ts
+++ src/app/home/main-menu/student-union/event/event.service.ts
@@ -1,9 +1,61 @@
+/* eslint-disable max-len */
 import { Injectable } from '@angular/core';
+import { Event } from './event.model';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EventService {
+
+  private _event: Event[] = [
+    new Event(
+      0,
+      'victor',
+      'Movember',
+      'Uni Event',
+      'Non - music',
+      'Imagine this.... you’re in the Refectory with your teammates, everyones dressed as well-known Heros and Villains, your sports captains and members are battling it out in a game of darts while you win points for your team by taking part in various challenges.... OH, then imagine there’s a 150” pair of balls hanging from the ceiling ',
+      0,
+      'Refectory, Fulton House, Singleton Camppus'
+    ),
+    new Event(
+      1,
+      'victor',
+      'Movember',
+      'Uni Event',
+      'Non - music',
+      'Imagine this.... you’re in the Refectory with your teammates, everyones dressed as well-known Heros and Villains, your sports captains and members are battling it out in a game of darts while you win points for your team by taking part in various challenges.... OH, then imagine there’s a 150” pair of balls hanging from the ceiling ',
+      10,
+      'Refectory, Fulton House, Singleton Camppus'
+    ),
+    new Event(
+      2,
+      'victor',
+      'Movember',
+      'Uni Event',
+      'Non - music',
+      'Imagine this.... you’re in the Refectory with your teammates, everyones dressed as well-known Heros and Villains, your sports captains and members are battling it out in a game of darts while you win points for your team by taking part in various challenges.... OH, then imagine there’s a 150” pair of balls hanging from the ceiling ',
+      20,
+      'Bay Camppus'
+    ),
+    new Event(
+      3,
+      'victor',
+      'Movember',
+      'Uni Event',
+      'Non - music',
+      'Imagine this.... you’re in the Refectory with your teammates, everyones dressed as well-known Heros and Villains, your sports captains and members are battling it out in a game of darts while you win points for your team by taking part in various challenges.... OH, then imagine there’s a 150” pair of balls hanging from the ceiling ',
+      30,
+      'Town square'
+    ),
+  ];
+
+  get event() {
+    // eslint-disable-next-line no-underscore-dangle
+    return [...this._event];
+  }
+
 
   constructor() { }
 }
