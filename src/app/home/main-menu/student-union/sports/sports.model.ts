81f4b8320009f0bd6ffbe9a27c5fdcefcdc5ef17
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-22 13:52:49 +0000

    created event model and schema

---
+++ src/app/home/main-menu/student-union/sports/sports.model.ts
@@ -0,0 +1,30 @@
+export class Societies {
+  constructor(
+    public id: number,
+    public img: string,
+    public title: string,
+    public category: string,
+    public description: string,
+    public trainingTime: TrainingTime[],
+    public events: string[],
+    public contact: Contacts,
+    ) {}
+}
+
+export class TrainingTime {
+  constructor(
+    public time: string,
+    public location: string,
+    ) {}
+}
+
+
+
+export class Contacts {
+  constructor(
+    public email: string,
+    public linkedin: string,
+    public facebook: string,
+    public instagram: string,
+    ) {}
+}
