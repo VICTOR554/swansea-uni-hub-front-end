24ee242e8aca9489b9e2e1d35e3a0748fd5d8725
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-12-10 05:04:27 +0000

    commented note crud and model to avoid error

--- src/app/home/notes/notes.model.ts
+++ src/app/home/notes/notes.model.ts
@@ -1,28 +1,28 @@
+export class Note {
+  constructor(
+      public id: number,
+      public title: string,
+      public moduleName: string,
+      public moduleCode: string,
+      public body: string,
+      public time: string,
+  ) { }
+}
+
 // export class Note {
 //   constructor(
-//       public id: number,
 //       public title: string,
-//       public moduleName: string,
 //       public moduleCode: string,
-//       public body: string,
-//       public time: string,
+//       public createdDateTime: number,
+//       public description: string,
+//       public _id?: string,
 //   ) { }
 // }
 
-export class Note {
-  constructor(
-      public title: string,
-      public moduleCode: string,
-      public createdDateTime: number,
-      public description: string,
-      public _id?: string,
-  ) { }
-}
-
-export class Module {
-  constructor(
-      public code: string,
-      public name: string,
-      public courseCode: number,
-  ) { }
-}
+// export class Module {
+//   constructor(
+//       public code: string,
+//       public name: string,
+//       public courseCode: number,
+//   ) { }
+// }
--- src/app/home/notes/notes.service.ts
+++ src/app/home/notes/notes.service.ts
@@ -11,95 +11,95 @@ import * as moment from 'moment';
 })
 export class NotesService {
 
-  // private _notes: Note [] = [
-  //   new Note(
-  //     1,
-  //     'HCI REVIEW',
-  //     'HCI',
-  //     'CSCM39',
-  //     // eslint-disable-next-line max-len
-  //     'Human-Computer Interaction is a discipline concerned with Design, Evaluation,and Implementation of interactive computer systems for human use, and◦with the study of the major phenomena surrounding them',
-  //     '10 am'
-  //   ),
-  //   new Note(
-  //     2,
-  //     'Block',
-  //     'Blockchain',
-  //     'CSCM29',
-  //     'The next block is protected by the transaction id',
-  //     '12 pm'
-  //   ),
-  //   new Note(
-  //     3,
-  //     'Etherium',
-  //     'Blockchain',
-  //     'CSCM29',
-  //     'The contract is vulnerable to attacks if the wrong element are utilised',
-  //     '1:30 pm'
-  //   ),
-  //   new Note(
-  //     4,
-  //     'Input verification',
-  //     'Software Testing',
-  //     'CSCM64',
-  //     'By looking at  the input and output of the system the user can verify, if the project is working as expected',
-  //     '4 pm'
-  //   ),
-  // ];
+  private _notes: Note [] = [
+    new Note(
+      1,
+      'HCI REVIEW',
+      'HCI',
+      'CSCM39',
+      // eslint-disable-next-line max-len
+      'Human-Computer Interaction is a discipline concerned with Design, Evaluation,and Implementation of interactive computer systems for human use, and◦with the study of the major phenomena surrounding them',
+      '10 am'
+    ),
+    new Note(
+      2,
+      'Block',
+      'Blockchain',
+      'CSCM29',
+      'The next block is protected by the transaction id',
+      '12 pm'
+    ),
+    new Note(
+      3,
+      'Etherium',
+      'Blockchain',
+      'CSCM29',
+      'The contract is vulnerable to attacks if the wrong element are utilised',
+      '1:30 pm'
+    ),
+    new Note(
+      4,
+      'Input verification',
+      'Software Testing',
+      'CSCM64',
+      'By looking at  the input and output of the system the user can verify, if the project is working as expected',
+      '4 pm'
+    ),
+  ];
 
-  // get notes() {
-  //   return [...this._notes];
+  get notes() {
+    return [...this._notes];
+  }
+  constructor() { }
+
+  getNote(id: string) {
+    return {...this._notes.find(p => p.id)};
+  }
+
+
+  // constructor(private authService: AuthService, private http: HttpClient) { }
+  // getAllNotes() {
+  //   return this.http.get('http://swansea-uni-hub-api.herokuapp.com/student/note/', this.authService.httpOptions);
   // }
-  // constructor() { }
 
-  // getNote(id: string) {
-  //   return {...this._notes.find(p => p.id)};
+  // getOneNote(noteId: string) {
+  //   console.log(noteId);
+  //   return this.http.get('http://swansea-uni-hub-api.herokuapp.com/student/note/one/' + noteId, this.authService.httpOptions);
+
+  // }
+  // getModule(moduleCode: string) {
+  //   return this.http.get('http://swansea-uni-hub-api.herokuapp.com/student/module/' + moduleCode, this.authService.httpOptions);
   // }
 
+  // addNote(title: string, moduleCode: string, description: string) {
+  //   // tslint:disable-next-line: variable-name
+  //   const dateTime = +moment().format('X');
+  //   console.log(this.authService.httpOptions.headers);
+  //   const newNote = new Note(
+  //     title,
+  //     moduleCode,
+  //     dateTime,
+  //     description,
+  //   );
+  //   return this.http.post('http://swansea-uni-hub-api.herokuapp.com/student/note/new', newNote, this.authService.httpOptions);
+  // }
 
-  constructor(private authService: AuthService, private http: HttpClient) { }
-  getAllNotes() {
-    return this.http.get('http://swansea-uni-hub-api.herokuapp.com/student/note/', this.authService.httpOptions);
-  }
+  // updateNote(title: string, moduleCode: string, description: string, noteId: string) {
+  //   // tslint:disable-next-line: variable-name
+  //   const dateTime = +moment().format('X');
+  //   console.log(this.authService.httpOptions.headers);
+  //   const updatedNote = new Note(
+  //     title,
+  //     moduleCode,
+  //     dateTime,
+  //     description,
 
-  getOneNote(noteId: string) {
-    console.log(noteId);
-    return this.http.get('http://swansea-uni-hub-api.herokuapp.com/student/note/one/' + noteId, this.authService.httpOptions);
+  //   );
+  //   return this.http.put('http://swansea-uni-hub-api.herokuapp.com/student/note/edit/' + noteId, updatedNote, this.authService.httpOptions);
+  // }
 
-  }
-  getModule(moduleCode: string) {
-    return this.http.get('http://swansea-uni-hub-api.herokuapp.com/student/module/' + moduleCode, this.authService.httpOptions);
-  }
-
-  addNote(title: string, moduleCode: string, description: string) {
-    // tslint:disable-next-line: variable-name
-    const dateTime = +moment().format('X');
-    console.log(this.authService.httpOptions.headers);
-    const newNote = new Note(
-      title,
-      moduleCode,
-      dateTime,
-      description,
-    );
-    return this.http.post('http://swansea-uni-hub-api.herokuapp.com/student/note/new', newNote, this.authService.httpOptions);
-  }
-
-  updateNote(title: string, moduleCode: string, description: string, noteId: string) {
-    // tslint:disable-next-line: variable-name
-    const dateTime = +moment().format('X');
-    console.log(this.authService.httpOptions.headers);
-    const updatedNote = new Note(
-      title,
-      moduleCode,
-      dateTime,
-      description,
-
-    );
-    return this.http.put('http://swansea-uni-hub-api.herokuapp.com/student/note/edit/' + noteId, updatedNote, this.authService.httpOptions);
-  }
-
-  deleteNote(noteId: string) {
-    return this.http.delete('http://swansea-uni-hub-api.herokuapp.com/student/note/delete/' + noteId, this.authService.httpOptions);
-  }
+  // deleteNote(noteId: string) {
+  //   return this.http.delete('http://swansea-uni-hub-api.herokuapp.com/student/note/delete/' + noteId, this.authService.httpOptions);
+  // }
 
 }
