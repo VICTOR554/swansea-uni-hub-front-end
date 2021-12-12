a8eafe0e5971518906192eeb10f15ce77608e7ed
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-12-12 18:23:28 +0000

    Created get methods in timetables service to collect get all activity, module, current week, all week, location and week by number

--- src/app/home/timetable/timetable.service.ts
+++ src/app/home/timetable/timetable.service.ts
@@ -1,63 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Activity } from './timetable.model';
+import * as moment from 'moment';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TimetableService {
 
-  private _activity: Activity [] = [
-    new Activity(
-      1,
-      'HCI',
-      'Lab',
-      '10 am',
-      '11 am',
-      'david',
-      'ENG',
-    ),
-    new Activity(
-      2,
-      'Block chain',
-      'Lecture',
-      '12 pm',
-      '1 pm',
-      'sam',
-      'DPD',
-    ),
-    new Activity(
-      3,
-      'Data visualisation',
-      'Seminar',
-      '2 pm',
-      '4 pm',
-      'conor',
-      'FFS',
-    ),
-    new Activity(
-      4,
-      'Software Testing',
-      'Lab',
-      '5 pm',
-      '6 pm',
-      'david',
-      'FMO',
-    ),
-    new Activity(
-      5,
-      'HCI',
-      'Lab',
-      '7 pm',
-      '8 pm',
-      'david',
-      'NPO',
-    ),
-  ];
+  constructor(private authService: AuthService, private http: HttpClient) { }
 
-  constructor() { }
-
-  get activity() {
-    // eslint-disable-next-line no-underscore-dangle
-    return [...this._activity];
+  GetAllActivity(currentDay: number) {
+    return this.http.get('https://swansea-uni-hub-api.herokuapp.com/student/activity/day/' + currentDay, this.authService.httpOptions);
   }
+
+  GetModule(moduleCode: string) {
+    return this.http.get('https://swansea-uni-hub-api.herokuapp.com/student/module/' + moduleCode, this.authService.httpOptions);
+  }
+
+  GetCurrentWeek() {
+    const createdDateTime = +moment().format('X');
+    console.log('Created_date_time', createdDateTime);
+    return this.http.get('https://swansea-uni-hub-api.herokuapp.com/student/week/date/' + createdDateTime, this.authService.httpOptions);
+  }
+
+  getAllWeeks() {
+    return this.http.get('https://swansea-uni-hub-api.herokuapp.com/student/week/', this.authService.httpOptions);
+  }
+
+  getLocation(key) {
+    return this.http.get('https://swansea-uni-hub-api.herokuapp.com/student/student/location/' + key, this.authService.httpOptions);
+  }
+
+  GetWeekByNumber(weekNumber: number) {
+    console.log('WeekNumber', weekNumber);
+    return this.http.get('https://swansea-uni-hub-api.herokuapp.com/student/student/week/number/' + weekNumber, this.authService.httpOptions);
+  }
+
 }
